9981bbec426bc70e4b27053bdc12c10f583913fc
Ping Zhao <ping@zhao.no>
2024-09-09 00:10:19 +0200

    Add a rust solution to 282a bit++

---
+++ 0282a_bit++/solution.rs
@@ -0,0 +1,47 @@
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, BufWriter, Write};
+
+#[derive(Default)]
+struct Scanner {
+    buffer: Vec<String>,
+}
+
+impl Scanner {
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parsing");
+            }
+
+            let mut input = String::new();
+            stdin().read_line(&mut input).expect("Failed reading");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+
+    #[allow(dead_code)]
+    fn nexts<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        return (0..n).map(|_| self.next()).collect();
+    }
+}
+
+fn solution() {
+    let mut scanner = Scanner::default();
+    let out = &mut BufWriter::new(stdout());
+
+    writeln!(
+        out,
+        "{}",
+        (0..scanner.next::<usize>())
+            .map(|_| match scanner.next::<String>().contains('+') {
+                true => 1,
+                false => -1,
+            })
+            .sum::<i64>()
+    )
+    .ok();
+}
+
+fn main() {
+    solution();
+}
